0855963100ec46e6c0c1ca95cea020b06dab6a93
trimai <mhtri02@gmail.com>
2023-08-04 08:04:49 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+	var typed = new Typed('.element', {
+          strings: ["Web Designer.", "Code Developer.","Apps Designer.","Film Maker"],
+       typeSpeed:20,
+      backSpeed:20,
+       loop:true,
+     });
+
+$('.counter').countUp();
